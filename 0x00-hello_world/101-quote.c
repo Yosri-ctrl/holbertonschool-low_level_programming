723259f277044fc41146bcb59ebfc4876631a0d0
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-05 16:26:37 +0100

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,7 @@
  */
 int main(void)
 {
-char c;
-c="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+int c="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 write(2,"and that piece of art is useful - Dora Korpar, 2015-10-19\n",s);
 return (0);
 }
