bbba206caa421fe9519d743482cf03a110ab5ea5
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-05 13:41:45 +0100

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ afiicher un message 
+ *
+return 0
+*/
 int main()
 {	
-	puts ("Programming is like building a multilingual puzzle",/n);
+	puts ("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
