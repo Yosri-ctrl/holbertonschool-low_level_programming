bc1d82dbedce7b4663d50f7f528293f97732a26a
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-05 15:04:55 +0100

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf(sizeof(intType));
+printf(sizeof(floatType));
+printf(sizeof(doubleType));
+printf(sizeof(charType));
+return (0);
+}
