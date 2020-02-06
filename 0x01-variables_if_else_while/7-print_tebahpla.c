2a9112263ec3532b72eefea10b5056e1126068ab
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-06 16:20:18 +0100

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 'z' ; c >= 'a' ; c--)
+	{
+		putchar(c);
+		putchar('\n');
+	}
+	return (0);
+}
