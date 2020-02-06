aad02e7d8fc2fba6eb48d4764476cad4837a1175
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-06 16:31:31 +0100

    a

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x;
+	
+	for ( x = '1' ; x <= '9' ; x++)
+	       putchar(x);
+	for ( x = 'a' ; x <= 'g' ; x++)
+	putchar(x);
+	return (0);
+}	
