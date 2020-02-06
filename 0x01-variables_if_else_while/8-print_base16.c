7e34f5cbbf9b2bb4e88fec2a434369f3a6c774bd
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-06 19:01:16 +0000

    8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,10 +9,12 @@
 int main(void)
 {
 	int x;
-	
-	for ( x = '1' ; x <= '9' ; x++)
-	       putchar(x);
-	for ( x = 'a' ; x <= 'g' ; x++)
+
+	for (x = '1' ; x <= '9' ; x++)
 	putchar(x);
+	for (x = 'a' ; x <= 'g' ; x++)
+	{putchar(x);}
+	putchar('\n');
+
 	return (0);
-}	
+}
