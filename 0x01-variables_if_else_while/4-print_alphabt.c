6ef43116869df92c823f0a4279d7913e4d4b400f
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-06 15:42:02 +0100

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,15 +1,17 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	char c;
 
-	for(c='a';c<='z';c++)
-		if((c!='e')&&(c!='q'))
-			putchar(c);
+	for (c = 'a'; c <= 'z'; c++)
+	if ((c != 'e') && (c != 'q'))
+		{putchar(c); }
 	putchar('\n');
 	return (0);
 }
