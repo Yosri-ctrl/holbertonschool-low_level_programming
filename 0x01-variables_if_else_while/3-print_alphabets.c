d38cbc5803d2b7b63c5c8c3a9711f6e0540da795
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-06 15:24:16 +0100

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,17 @@
 #include <stdblib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
 int main(void)
 {
 	char c;
 
-	for(c='a';c<='z';c++)
+	for(c = 'a';c <= 'z'; c++)
 		putchar(c);
-	for(c='A';c<='Z';c++)
+	for(c = 'A';c <= 'Z'; c++)
 		putchar(c);
 	putchar('\n');
 	return(0);
