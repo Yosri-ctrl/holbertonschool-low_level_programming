55c20479b3abbfbb398172ae6d14965a8dc6fab5
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-06 20:18:35 +0000

    101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i, a, b, c;
+
+	for (i = 0; i < 1000; i++)
+	{
+	a = i / 100;
+	b = (i / 10) % 10;
+	c = i % 10;
+
+	if (a < b && b < c)
+	{
+	putchar(a + '0');
+	putchar(b + '0');
+	putchar(c + '0');
+
+	if (i < 700)
+	{
+	putchar(44);
+	putchar(32);
+	}
+	}
+	}
+	putchar('\n');
+
+	return (0);
+}
