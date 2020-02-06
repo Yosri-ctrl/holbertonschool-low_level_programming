e0a10be04fdeb7e9b07a6b56cb84e8415ae3c868
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-06 16:09:39 +0100

    6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-	int x;
-	
-	for (	
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		putchar('\n');
+	}
+	return (0);
 }
