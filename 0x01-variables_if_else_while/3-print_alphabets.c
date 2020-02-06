12aae18220ddceb140843d8f5511983c689ac307
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-06 15:34:26 +0100

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,14 +5,15 @@
  * main - Entry point
  *
  * Return: Always 0 (success)
+ */
 int main(void)
 {
 	char c;
 
-	for(c = 'a';c <= 'z'; c++)
-		putchar(c);
-	for(c = 'A';c <= 'Z'; c++)
-		putchar(c);
+	for (c = 'a'; c <= 'z'; c++)
+	{putchar(c); }
+	for (c = 'A'; c <= 'Z'; c++)
+	{putchar(c) ; }
 	putchar('\n');
-	return(0);
+	return (0);
 }
