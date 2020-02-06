bec0a3f1f70c639b79b7a2cf8d5082657833c1a2
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-06 20:10:39 +0000

    10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int i;
+int j;
+
+for (i = 0 ; i <= 9 ; i++)
+{
+for (j = 0 ; j <= 9 ; j++)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(44);
+putchar(32);
+}
+}
+
+return  (0);
+}
