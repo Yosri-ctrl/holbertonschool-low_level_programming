f7adc3797e4bf0ed4c906277795c6898cb3ae39b
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-06 15:54:42 +0100

    5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,16 @@
 #include <stdlib.h>
 #include <time.h>
-/* more header goes ther */
-
-/* betty style doc for function main goes there */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	int x;
 
-	for (x=0;x<10;x++)
-		printf("%d\n",x);
+	for (x = 0; x < 10; x++)
+		printf("%d\n", x);
 	return (0);
 }
