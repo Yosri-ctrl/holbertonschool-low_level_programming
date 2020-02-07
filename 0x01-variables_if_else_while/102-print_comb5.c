74461cd69eee0d5cb5acb0828f6908539278d825
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-07 20:48:27 +0000

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,7 +9,7 @@ int main(void)
 	int i, j;
 
 	for (i = 0; i < 100; i++)
-	for (j = 0; j < 100; j++)
+	for (j = i+1; j < 100; j++)
 	{
 	if (i != j)
 	{
@@ -18,7 +18,7 @@ int main(void)
 	putchar(' ');
 	putchar((j / 10) + '0');
 	putchar((j % 10) + '0');
-	if (i != 99 || i != 99)
+	if (i != 98 || i != 99)
 	{
 	putchar(',');
 	putchar(' ');
