9cdef30e43d756e92f98f16c3083798cd382b821
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-07 11:17:22 +0000

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@
 int main(void)
 {
 int c = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", c);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", c);
 return (1);
 }
