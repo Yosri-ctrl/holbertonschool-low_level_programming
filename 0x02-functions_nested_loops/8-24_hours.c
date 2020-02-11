b34174e28a2d2da897853726e202808f3a95ca00
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-11 15:03:41 +0100

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,21 +4,18 @@
  *
  * Return: Always 0 (success)
  */
-int jack_bauer(void)
+void jack_bauer(void)
 {
 int i, j;
 
 for (i = 0 ; i < 24 ; i++)
-{
-
 for (j = 0 ; j < 60 ; j++)
 {
 _putchar(i / 10 + '0');
 _putchar(i % 10 + '0');
 _putchar(':');
 _putchar(j / 10 + '0');
-_putchar(j % 10 + '0's);
+_putchar(j % 10 + '0');
 _putchar('\n');
-}
 }
 }
