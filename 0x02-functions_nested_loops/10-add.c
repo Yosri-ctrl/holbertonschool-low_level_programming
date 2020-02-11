684f0e7fa5b990c51abea75dcb89583a7c268e1d
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-11 16:54:12 +0100

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * add - Entry point
+ * @a : is a number
+ * @b : is a number
+ * Return: Always 0 (success)
+ */
+int add(int a, int b)
+{
+_putchar((a + b) + '0');
+return (a + b);
+}
