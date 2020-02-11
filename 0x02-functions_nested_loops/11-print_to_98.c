34b69a805f2c5021f9fba6e757633dbe5d1b2924
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-11 18:22:23 +0100

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,6 @@
 /**
  * print_to_98 - Entry point
  * @a : is a number
- * @b : is a number
  * Return: Always 0 (success)
  */
 int print_to_98(int a)
@@ -11,7 +10,7 @@ int print_to_98(int a)
 if (a < 98)
 for (; a <= 98; a++)
 {
-_printf("%d",a);
+_printf("%d", a);
 if (a != 98)
 _putchar(',');
 }
