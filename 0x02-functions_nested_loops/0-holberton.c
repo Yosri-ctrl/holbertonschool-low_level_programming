728b04e590eff1cc548e6e9ea146fa4eec02bc6d
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-11 09:26:05 +0100

    0

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+  char *c;
+  int i;
+
+  for (i = 0 ; i <= strlen(*c) ; i++)
+  _putchar(i);
+
+  return(0);
+}
