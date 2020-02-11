982a5d1faa6f67abe15b9835f9de686e5021011e
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-11 18:50:20 +0100

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,20 +7,8 @@
  */
 void print_to_98(int n)
 {
-if (n > 98)
-{
-while (n <= 98)
-{
-printf("%d", n);
-if (n != 98)
-{
-printf(", ");
-}
-n++;
-}
-}
 
-else
+if (n > 98)
 {
 while (n >= 98)
 {
@@ -30,6 +18,19 @@ if (n != 98)
 printf(", ");
 }
 n--;
+}
+}
+
+else
+{
+while (n <= 98)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
+n++;
 }
 }
 printf("\n");
