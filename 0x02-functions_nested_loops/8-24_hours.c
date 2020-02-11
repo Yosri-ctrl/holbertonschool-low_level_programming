bb7a2ab0dcbc702f744dbb83c32579c9e3fbf26c
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-11 15:02:41 +0100

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,13 +10,14 @@ int i, j;
 
 for (i = 0 ; i < 24 ; i++)
 {
-_putchar(i / 10);
-_putchar(i % 10);
-_putchar(':');
+
 for (j = 0 ; j < 60 ; j++)
 {
-_putchar(j / 10);
-_putchar(j % 10);
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+_putchar(':');
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0's);
 _putchar('\n');
 }
 }
