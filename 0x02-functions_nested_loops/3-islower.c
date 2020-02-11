8de27abd9d675df8696c582cf8f62d36ea19d78d
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-11 11:58:11 +0100

    3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,15 @@
 #include "holberton.h"
 /**
  * _islower - Entry point
+ * int c - is a chracter lower or upper
  * Return: Always 0 (success)
  */
 int _islower(int c)
 {
-if (c >= 97 && c <= 122)
-  return (1);
+if (*c >= 97 && *c <= 122)
+{
+return (1);
+}
 else
-  return (0);
+return (0);
 }
