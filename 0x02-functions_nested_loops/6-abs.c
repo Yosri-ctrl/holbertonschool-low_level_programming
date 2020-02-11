73883b3f965f0e36f921987264762313c9702612
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-11 13:49:44 +0100

    6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,15 @@
 #include "holberton.h"
 /**
  * _abs - Entry point
- * @int : is a number
+ * @n : is a number
  * Return: Always 0 (success)
  */
-int _abs(int)
+int _abs(int n)
 {
 if (n < 0)
+{
 _putchar(n * -1);
+}
 else
 _putchar(n)
 }
