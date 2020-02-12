3c61f871827ebf9eef4ba74d5e961e9b37893f5c
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-12 10:09:01 +0100

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,14 +6,14 @@
  */
 int print_last_digit(int n)
 {
-if (x >= 0)
+if (n >= 0)
 {
-_putchar(x % 10 + '0');
-return (x % 10);
+_putchar(n % 10 + '0');
+return (n % 10);
 }
 else
 {
-_putchar((x % 10) * -1 + '0');
-return ((x % 10) * -1);
+_putchar((n % 10) * -1 + '0');
+return ((n % 10) * -1);
 }
 }
