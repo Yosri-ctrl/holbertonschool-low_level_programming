09f2ae3c1dd2edc47e514fac6cc0f7f472c1e2bb
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-12 20:44:45 +0100

    104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Print the first 98 Fibonacci numbers.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int i;
+long int x = 1, y = 2, n, s = 0;
+
+printf("%lu, ", x);
+for (i = 2; i <= 98; i++)
+{
+if (i != 98)
+printf("%lu, ", y);
+else
+printf("%lu", y);
+
+n = y;
+y += x;
+x = n;
+}
+
+return (0);
+}
