962cdcab9ddfca5feff67411ca8d58b0a5ed45bd
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-13 13:37:19 +0100

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 /**
  * print_most_numbers - print num exept 2 & 4.
+ *
  * Return: Always 0 (success)
  */
 void print_most_numbers(void)
 {
 int i;
 for (i = 0 ; i <= 9 ; i++)
-{
 if (i != 2 && i != 4)
 _putchar (i + '0');
-}
+
 _putchar ('\n');
 }
