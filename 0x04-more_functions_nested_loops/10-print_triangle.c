32d6524be6d8c9aab36f805717986def1cb42cf6
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-13 22:49:12 +0100

    10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,6 +8,10 @@ void print_triangle(int size)
 {
 int i, j, k;
 
+if (size < 0)
+_putchar('\n');
+else
+{
 for (i = 0; i < size; i++)
 {
 for (j = i + 1; j < size; j++)
@@ -17,5 +21,6 @@ for (k = 0; k <= i; k++)
 _putchar('#');
 
 _putchar('\n');
+}
 }
 }
