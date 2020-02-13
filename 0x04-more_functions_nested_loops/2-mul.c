af013531e33bd745d5af8ab9d98939390e9963a3
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-13 13:11:53 +0100

    1

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * _isupper - check chracters upper or lower.
+ * mul - check chracters upper or lower.
  * @a : the first num.
  * @b : the second num.
  * Return: Always 0 (success)
