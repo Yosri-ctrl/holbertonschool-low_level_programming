24e098c27f3e0b279fcfa7f17da068ac75a0c8cc
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-13 13:29:36 +0100

    1

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,17 @@
 #include "holberton.h"
 /**
- * print_numbers - print from 0 to 9.
- * Return: Always 0 (success)
+ * print_numbers - print the numbers from 0 to 9
+ *
+ * Description: You can only use _putchar twice
  */
 void print_numbers(void)
 {
 int i;
-for (i = '0'; i <= '9' ; i++)
-_putchar(i);
-
+i = 0;
+while (i < 10)
+{
+_putchar(i + '0');
+i++;
+}
 _putchar('\n');
 }
