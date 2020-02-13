a264216ac15da99d9f28e45163de391a49008479
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-13 13:02:45 +0100

    1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 /**
- * _isdigit - check if it is a number.
- * @c : the number.
- * Return: Always 0 (success)
- */
+* _isdigit - check if it is a number.
+* @c : the number.
+* Return: Always 0 (success)
+*/
 int _isdigit(int c)
 {
 if (c >= 0 && c <= 9)
 return (1);
 else
-printf(0);
+return (0);
 }
