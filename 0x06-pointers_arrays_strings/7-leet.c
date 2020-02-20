beb295f13c01043708494a4884b902f6f2423a29
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-20 21:01:45 +0100

    7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+* leet -  function that encodes a string into 1337.
+* @c : the distination
+* Return: Always 0 (success)
+*/
+char *leet(char *c)
+{
+int i;
+
+for (i = 0; c[i] != '\0'; i++)
+{
+while ((c[i] == 'o') || (c[i] == 'O'))
+c[i] = '0';
+while ((c[i] == 'l') || (c[i] == 'L'))
+c[i] = '1';
+while ((c[i] == 'e') || (c[i] == 'E'))
+c[i] = '3';
+while ((c[i] == 'a') || (c[i] == 'A'))
+c[i] = '4';
+while ((c[i] == 't') || (c[i] == 'T'))
+c[i] = '7';
+}
+return (c);
+}
