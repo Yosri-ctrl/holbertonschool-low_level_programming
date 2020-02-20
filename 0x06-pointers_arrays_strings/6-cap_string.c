c2d69ac8a164d2f0e0b749e482246b76cbb09509
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-20 20:22:07 +0100

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,15 +6,19 @@
 */
 char *cap_string(char *c)
 {
-int i, j;
+int i, j, k, m;
+char x[] = " \n\t,.:\"(){}?!";
 
 for (i = 0; c[i] != '\0'; i++)
 ;
+for (m = 0; x[m] != '\0'; m++)
+;
+
 for (j = 0; j < i; j++)
 {
-if ((c[j] >= 'a' && c[j] <= 'z') && (c[j - 1] == ' ' || c[j - 1] == '\t' || c[j -1] == '\n'))
+for (k = 0; k < m; k++)
+if ((c[j] >= 'a' && c[j] <= 'z') && (c[j - 1] == x[k]))
 c[j] = c[j] - 32;
-
 }
 return (c);
 }
