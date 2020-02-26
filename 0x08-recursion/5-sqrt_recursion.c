18b27327742212b8469d28aa89f4bdef36c52b4f
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-26 22:01:33 +0100

    5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,26 +1,28 @@
 #include "holberton.h"
 /**
 * _sqrt_recursion - Write a function that print the sqrt
+* _second - Counter for _sqrt_recursion
 *@n : the numb
+*@x : the counter
 *Return: Always 0
 */
-int second (int n, int x);
+int _second(int n, int x);
 
 int _sqrt_recursion(int n)
 {
-  int x = 1;
-  return (second(n, x));
+int x = 1;
+return (_second(n, x));
 }
 
-int second(int n, int x)
+int _second(int n, int x)
 {
-  int r;
-  r =  x * x;
+int r;
+r =  x * x;
 
-  if (r == n)
-  return(x);
-  else if (r < n)
-  return (second(n, ++x));
-  else
-  return (-1);
+if (r == n)
+return (x);
+else if (r < n)
+return (_second(n, ++x));
+else
+return (-1);
 }
