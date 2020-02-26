347477bdc003e529eea3553ac920c9411df33032
Yosri-ctrl <yosribouabid@gmail.com>
2020-02-26 21:42:27 +0100

    0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* _puts_recursion - Write a function that prints a string
+*@s : the char
+*Return: Always 0
+*/
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+_putchar('\n');
+else
+{
+_putchar(*s);
+_puts_recursion(++s);
+}
+}
