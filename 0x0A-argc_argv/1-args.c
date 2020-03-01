9e4e592a606ff9fca4f5057f9e9fc12127deecea
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-01 12:49:20 +0100

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - print the nbr arguments
+*@argc : nbr of parametre
+*@argv : array holing the paranetre
+*Return: Always 0
+*/
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc);
+return (0);
+}
