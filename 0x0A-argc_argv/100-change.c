a5c7710549aa1bff177304d1ba07fb411048e968
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-01 15:23:04 +0100

    100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,38 +9,37 @@
 */
 int main(int argc, char *argv[])
 {
-  int x = 0, v;
-  int c[10] = {25, 10, 5, 2, 1};
-  unsigned i;
-  (void)argc;
+int x = 0, v;
+int n[10] = {25, 10, 5, 2, 1};
+int i;
 
-  if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
 
-  if (atoi(argv[1]) <= 0)
-	{
-		printf("0\n");
-		return (1);
-	}
+if (atoi(argv[1]) <= 0)
+{
+printf("0\n");
+return (1);
+}
 
-  v = atoi(argv[1]);
+v = atoi(argv[1]);
 
-  while(v != 0)
-  {
-    for (i = 0; i < 5; i++)
-    {
-    if (v >= c[i])
-    {
-      v -= c[i];
-      x++;
-      break;
-    }
-    }
-  }
-  printf("%d\n", x);
+while (v != 0)
+{
+for (i = 0; i < 5; i++)
+{
+if (v >= n[i])
+{
+v -= n[i];
+x++;
+break;
+}
+}
+}
+printf("%d\n", x);
 
-  return (0);
+return (0);
 }
