d2de3ceef335f5854417bb6c16a1d2c2157ec015
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-03 21:46:02 +0100

    3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,12 +18,12 @@ a = malloc(height * sizeof(*a));
 if (a == NULL)
 return (NULL);
 
-for (i = 0; i < height ;i++)
+for (i = 0; i < height; i++)
 {
 a[i] = malloc(width * sizeof(*a));
 if (a[i] == NULL)
 {
-free (a);
+free(a);
 return (NULL);
 }
 for (j = 0; j < width; j++)
