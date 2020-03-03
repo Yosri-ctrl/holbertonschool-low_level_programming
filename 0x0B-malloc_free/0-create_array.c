150e3bfdbfd355a84785519d9df755a78ecad4f0
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-03 19:06:10 +0100

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,19 +8,19 @@
 */
 char *create_array(unsigned int size, char c)
 {
-  unsigned int i;
-  char *a;
+unsigned int i;
+char *a;
 
-  if (size == 0)
-  return NULL;
+if (size == 0)
+return NULL;
 
-  a = malloc(size * sizeof(*a));
+a = malloc(size * sizeof(*a));
 
-  if (a == NULL)
-  return NULL;
+if (a == NULL)
+return NULL;
 
-  for (i = 0; i < size; i++)
-  a[i] = c;
+for (i = 0; i < size; i++)
+a[i] = c;
 
-  return (a);
+return (a);
 }
