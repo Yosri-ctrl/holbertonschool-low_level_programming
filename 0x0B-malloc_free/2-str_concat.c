82977ec4f08ee81eb879267275e0d80d47b77ed7
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-03 21:08:29 +0100

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,29 +8,29 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-  int i, j, k;
-  int x, y;
-  char *c;
+int i, j, k;
+int x, y;
+char *c;
 
-  if (s1 == NULL)
-  s1 = "";
-  if (s2 == NULL)
-  s2 = "";
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 
-  for (x = 0; s1[x] != '\0'; x++)
-  ;
-  for (y = 0; s2[y] != '\0'; y++)
-  ;
+for (x = 0; s1[x] != '\0'; x++)
+;
+for (y = 0; s2[y] != '\0'; y++)
+;
 
-  c = malloc((x * sizeof(*s1)) + (y * sizeof(*s2)) + 1);
-  if (c == NULL)
-  return (NULL);
+c = malloc((x * sizeof(*s1)) + (y * sizeof(*s2)) + 1);
+if (c == NULL)
+return (NULL);
 
-  for (i = 0; i < x; i++)
-  c[i] = s1[i];
+for (i = 0; i < x; i++)
+c[i] = s1[i];
 
-  for (j = 0, k = x; k < x + y + 1; j++, k++)
-  c[k] = s2[j];
+for (j = 0, k = x; k < x + y + 1; j++, k++)
+c[k] = s2[j];
 
-  return (c);
+return (c);
 }
