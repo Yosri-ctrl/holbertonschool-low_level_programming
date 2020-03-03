1b3b402458b9551312d7cabf1781b28cabc362d2
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-03 19:44:44 +0100

    1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+*_strdup - duplicate an str
+*@str: the str
+*Return: Always 0
+*/
+char *_strdup(char *str)
+{
+  int i, j;
+  char *c;
+
+  if (str == NULL)
+  return (NULL);
+
+  for (j = 0; str[j] != '\0'; j++)
+  ;
+  c = malloc (j * sizeof(*c));
+
+  for (i = 0; i < j; i++)
+  c[i] = str[i];
+
+  return (c);
+}
