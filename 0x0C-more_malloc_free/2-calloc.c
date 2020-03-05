3bc6db438d5dc3b2187d99ba6293f05ac24b213c
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-05 21:41:32 +0100

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,6 +9,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
     int *a;
+    unsigned int i;
 
     if (nmemb == 0 || size == 0)
     return (NULL);
@@ -16,6 +17,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
     a = malloc(nmemb * size);
     if (a == NULL)
     return (NULL);
+
+    for (i = 0; i < nmemb; i++)
+    {
+        a[i] = 0;
+    }
 
     return (a);
 }
