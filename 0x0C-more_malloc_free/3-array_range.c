9940d8c423b7040f5cb4ca4423615f27311d08e9
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-05 22:31:51 +0100

    3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+*array_range - concatenate two strs
+*@min: min
+*@max: max
+*Return: Always 0
+*/
+int *array_range(int min, int max)
+{
+    int *a;
+    int i, j;
+
+    if (min > max)
+    return (NULL);
+
+    a = malloc((min * sizeof(int)) + (max * sizeof(int)) + 1);
+    if (a == NULL)
+    return (NULL);
+
+    for (i = min, j = 0; i <= max; i++, j++)
+    a[j] = i;
+
+    return(a);
+}
