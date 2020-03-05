b3ad7cd279ab87231e69e4d87330f93b00b61632
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-05 22:33:36 +0100

    3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,7 +14,7 @@ int *array_range(int min, int max)
     if (min > max)
     return (NULL);
 
-    a = malloc((min * sizeof(int)) + (max * sizeof(int)) + 1);
+    a = malloc(sizeof(int) * (max - min) + 1);
     if (a == NULL)
     return (NULL);
 
