05abcbf8ba1a40b798347fb786de3f3fc2e59356
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-05 21:29:03 +0100

    2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+*_calloc - concatenate two strs
+*@nmemb : the
+*@size : size
+*Return: Always 0
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+    int *a;
+
+    if (nmemb == 0 || size == 0)
+    return (NULL);
+
+    a = malloc((nmemb * size) * sizeof(int));
+    if (a == NULL)
+    return (NULL);
+
+    return (a);
+
+}
