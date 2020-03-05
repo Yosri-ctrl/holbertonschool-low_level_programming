eaccc4a7b6a2ded3d32598e6c643cdff7dd02256
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-05 19:43:42 +0100

    0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+*malloc_checked - allocate the memory
+*@b : the size
+*Return: Always 0
+*/
+void *malloc_checked(unsigned int b)
+{
+    char *c;
+
+    c = malloc(b);
+    if (c == NULL)
+    exit (98);
+
+    return (c);
+}
