78865db8c56e04fc262b47ea2767a0c0cb2111c6
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-09 14:24:14 +0000

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+*strucy dog - define dog
+*@name : the name of the dog
+*@age : the age
+*@owner : the owner
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
