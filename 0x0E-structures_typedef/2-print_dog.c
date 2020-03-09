227de1fcab1b49cc79d864514e4ac0d976181317
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-09 23:08:51 +0100

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,14 @@ printf("Name: (nil)");
 else
 printf("Name: %s\n", d->name);
 
+if (d->age == NULL)
+printf("Age: (nil)");
+else
 printf("Age: %f\n", d->age);
+
+if (d->owner == NULL)
+printf("Owner: (nil)");
+else
 printf("Owner: %s\n", d->owner);
 }
 }
