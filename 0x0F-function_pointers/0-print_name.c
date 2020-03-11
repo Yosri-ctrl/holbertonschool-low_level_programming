618a6ccca77415304d2977d3c3e04a1b75bf9e46
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-11 20:10:05 +0100

    0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,11 +2,11 @@
 /**
  * print_name - print a name
  * @name: the name to print
- * 
+ * @f : calling the either print_name_as_is or print_name_uppercase
  */
 void print_name(char *name, void (*f)(char *))
 {
-    void (*a)(char *);
-    a = *f;
-    a(name);
+void (*a)(char *);
+a = *f;
+a(name);
 }
