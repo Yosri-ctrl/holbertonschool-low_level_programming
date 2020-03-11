caf931f61df8804a929c7230cf10c41a04ee9afa
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-11 21:51:55 +0100

    1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,12 +4,14 @@
  * array_iterator - function that executes a function
  * @array: the array
  * @size : the size
- * @action : 
+ * @action : the fct to print
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 size_t i;
-
+if (array != NULL && action != NULL)
+{
 for (i = 0; i < size; i++)
 action(array[i]);
+}
 }
