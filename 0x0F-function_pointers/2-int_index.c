6f46419749a8b75644f83e294d8771d789f3e7cf
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-11 22:12:09 +0100

    2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,20 +5,22 @@
  * @array: the array
  * @size : the size
  * @cmp : the fct to print
+ * Return:  index of the first element
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-    int i;
-    if (size <= 0)
-    return (-1);
+int i;
 
-    if (array == NULL || cmp == NULL)
-    return (-1);
+if (size <= 0)
+return (-1);
 
-    for (i = 0; i < size; i++)
-    {
-        if (cmp(array[i]))
-        return (i);
-    }
-    return (-1);
+if (array == NULL || cmp == NULL)
+return (-1);
+
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]))
+return (i);
+}
+return (-1);
 }
