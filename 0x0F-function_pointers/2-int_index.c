9f903d1fdb7f402ad9a497b51fe7a348434936cc
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-11 22:08:57 +0100

    2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * int_index - function that executes a function
+ * @array: the array
+ * @size : the size
+ * @cmp : the fct to print
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+    int i;
+    if (size <= 0)
+    return (-1);
+
+    if (array == NULL || cmp == NULL)
+    return (-1);
+
+    for (i = 0; i < size; i++)
+    {
+        if (cmp(array[i]))
+        return (i);
+    }
+    return (-1);
+}
