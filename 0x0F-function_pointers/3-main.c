094cf7fe5aa23e6aea26a0706706d78e3ff2fe40
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-12 22:54:26 +0100

    3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - clac two nmb
+ * @argc: nmb of argv arg
+ * @argv:the arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+    int num1, num2;
+    int (*res)(int, int);
+
+    if (argc != 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+    if (argv[2][1] != '\0')
+    {
+        printf("Error\n");
+        exit(99);
+        }
+
+    res = get_op_func(argv[2]);
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[2]);
+    printf("%d\n", res(num1, num2));
+    
+    return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * op_add - addition
+ * @a : the first nmb
+ * @b : the second nmb
+ * Return : the resulte
+ */
+int op_add(int a, int b)
+{
+    return (a + b);
+}
+
+/**
+ * op_sub - difference
+ * @a : the first nmb
+ * @b : the second nmb
+ * Return : the resulte
+ */
+int op_sub(int a, int b)
+{
+    return (a - b);
+}
+
+/**
+ * op_mul - product
+ * @a : the first nmb
+ * @b : the second nmb
+ * Return : the resulte
+ */
+int op_mul(int a, int b)
+{
+    return (a * b);
+}
+
+/**
+ * op_div - result of division
+ * @a : the first nmb
+ * @b : the second nmb
+ * Return : the resulte
+ */
+int op_div(int a, int b)
+{
+    if (b != 0)
+    return (a / b);
+    else
+    {
+        printf("Error\n");
+        exit(100);
+    }
+    
+}
+
+
+/**
+ * op_mod - rest of division
+ * @a : the first nmb
+ * @b : the second nmb
+ * Return : the resulte
+ */
+int op_mod(int a, int b)
+{
+    if (b != 0)
+    return (a % b);
+    else
+    {
+        printf("Error\n");
+        exit(100);
+    }
+}
