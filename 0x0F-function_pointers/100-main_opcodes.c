bff2bb06f443037a0cd72a1fbcd022c055d193d1
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-12 23:14:31 +0100

    100

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,16 @@
+#include 
+/**
+ * main - print the opcodes of its own main function
+ * @argc: nbm of arguments in argv
+ * @argv: list of te arguments
+ */
+void main(int argc, char *argv[])
+{
+if (argc != 3)
+printf("Error");
+exit(1);
+
+if (argv[1] <= 0)
+printf ("Error");
+exit(2);
+}
