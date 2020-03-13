a060e57aa3488480bc6edc6683d1a00ec0749c83
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-13 11:07:46 +0100

    1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_numbers - print list of nbms
+ * @separator: ", "
+ * @n: the nmb
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+    va_list a;
+    unsigned int i;
+
+    va_start(a, n);
+    for (i = 0; i < n; i++)
+    {
+        printf("%d", va_arg(a, int));
+        if (separator != NULL)
+        {
+            if (i != n - 1)
+            printf("%s", separator);
+        }
+    }
+    va_end(a);
+    printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,7 @@
 #ifndef _VARIADIC_FUMCTIONS_H_
 #define  _VARIADIC_FUMCTIONS_H_
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+
 
 #endif
