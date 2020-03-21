b9ca381e7235b140026798438cfe5fea215e1e8c
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-21 13:25:38 +0100

    0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,28 +1,26 @@
 #include "lists.h"
 /**
  * print_list - print the list givin
- * @h: the list to treat
+ * @head: the list to treat
  * Return: the size of the list
  **/
 size_t print_list(const list_t *head)
 {
-    //const list_t *current = head;
-    int nbr_node;
 
-    for (nbr_node = 0; head != NULL;)
-    {
-        if (head->str == NULL)
-        {
-            printf("[0] (nil)\n");
-            //printf("[%d] %s\n", head->next->len, head->next->str);
-        }
-        else
-        {
-            printf("[%d] %s\n", head->len, head->str);
-            //printf("[%d] %s\n", head->next->len, head->next->str);
-        }
-        nbr_node++;
-        head = head->next;
-    }
-    return (nbr_node);
+int nbr_node;
+
+for (nbr_node = 0; head != NULL;)
+{
+if (head->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%d] %s\n", head->len, head->str);
+}
+nbr_node++;
+head = head->next;
+}
+return (nbr_node);
 }
