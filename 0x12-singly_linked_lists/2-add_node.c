61049ead88740a8cd406d3d300e82b0169ccd34a
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-22 19:43:52 +0100

    2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,29 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * print_list - print the list givin
+ * add_node - add_node
  * @head: the list to treat
- * Return: the size of the list
+ * @str: the str to add
+ * Return: the pointer to next node
  **/
 list_t *add_node(list_t **head, const char *str)
 {
-    int i;
-    list_t *new_head;
+int i;
+list_t *new_head;
 
-    new_head = malloc(sizeof(list_t));
-    if (new_head == NULL)
-    {
-        free(new_head);
-        return (NULL);
-    }
+new_head = malloc(sizeof(list_t));
+if (new_head == NULL)
+{
+free(new_head);
+return (NULL);
+}
 
-    for (i = 0; str[i] != '\0'; i++)
-    ;
+for (i = 0; str[i] != '\0'; i++)
+;
 
-    new_head->str = strdup(str);
-    new_head->len = i;
-    new_head->next = *head;
+new_head->str = strdup(str);
+new_head->len = i;
+new_head->next = *head;
 
-    *head = new_head;
-    return (*head);
+return (*new_head);
 }
