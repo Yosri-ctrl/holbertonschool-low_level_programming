de443f7f71cde104514ae911468a80741294bd16
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-22 22:41:47 +0100

    3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,37 +9,37 @@
  **/
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new;
-    list_t *c;
-    int i;
+list_t *new;
+list_t *c;
+int i;
 
 
 
-    for (i = 0; str[i] != '\0'; i++)
-    ;
+for (i = 0; str[i] != '\0'; i++)
+;
 
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-    {
-        free(new);
-        return (NULL);
-    }
+new = malloc(sizeof(list_t));
+if (new == NULL)
+{
+free(new);
+return (NULL);
+}
 
-    new->str = strdup(str);
-    new->len = i;
-    new->next = NULL;
+new->str = strdup(str);
+new->len = i;
+new->next = NULL;
 
-    c = *head;
+c = *head;
 
-    while (c != NULL && c->next != NULL)
-        c = c->next;
+while (c != NULL && c->next != NULL)
+c = c->next;
 
-    if (*head == NULL)
-    {
-        *head = new;
-        return (*head);
-    }
+if (*head == NULL)
+{
+*head = new;
+return (*head);
+}
 
-    c->next = new;
-    return (*head);
+c->next = new;
+return (*head);
 }
