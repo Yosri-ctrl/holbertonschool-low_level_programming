9017e353139abccde344c9e3520cf2619aea1c4b
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-24 22:23:27 +0100

    5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,8 +7,17 @@
  **/
 void free_listint2(listint_t **head)
 {
+    listint_t *node;
+
     if (head == NULL)
     return;
-    else
+    
+    node = *head;
+    if (node != NULL)
+    {
+        free(node->next);
+        free(node);
+    }
+
     *head = NULL;
 }
