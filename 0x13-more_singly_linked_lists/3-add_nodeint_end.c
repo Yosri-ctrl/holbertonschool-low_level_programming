a580d2f40dd8d9090e5b560598226d302396d8a1
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-24 22:08:59 +0100

    3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -2,33 +2,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * add_nodeint - add_nodeint
+ * add_nodeint_end - add_nodeint_end
  * @head: the head of the list
  * @n: the int to insert
  * Return: the pointer to the next node
  **/
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-    listint_t *new_node;
-    listint_t *node;
+listint_t *new_node;
+listint_t *node;
 
-    new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL)
-    return (NULL);
+new_node = malloc(sizeof(listint_t));
+if (new_node == NULL)
+return (NULL);
 
-    new_node->n = n;
-    new_node->next = NULL;
+new_node->n = n;
+new_node->next = NULL;
 
-    if (*head == NULL)
-    {
-        *head = new_node;
-        return (*head);
-    }
+if (*head == NULL)
+{
+*head = new_node;
+return (*head);
+}
 
-    node = *head;
-    while (node->next != NULL)
-        node = node->next;
-    node->next = new_node;
+node = *head;
+while (node->next != NULL)
+node = node->next;
+node->next = new_node;
 
-    return (*head);
+return (*head);
 }
