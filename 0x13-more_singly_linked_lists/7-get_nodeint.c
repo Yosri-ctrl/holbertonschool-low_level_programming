83fff5832afa7c1ab6be35a0ac9ef0270d82d294
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-24 23:16:56 +0100

    7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -2,20 +2,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * pop_listint - pop_listint
+ * get_nodeint_at_index - get_nodeint_at_index
  * @head: the head of the list
+ * @index: the node searching for
  * Return: the data
  **/
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-    unsigned int i;
+unsigned int i;
 
-    for (i = 0; i < index; i++)
-    {
-        if (head == NULL)
-        return;
-        head = head->next;
-    }
+for (i = 0; i < index; i++)
+{
+if (head->next == NULL)
+return;
+head = head->next;
+}
 
-    return (head);
+return (head);
 }
