d926131849bc6c104fe38438016367f29bd2fff3
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-24 19:53:27 +0100

    1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,15 +1,15 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- * print_listint - print_listint
+ * listint_len - listint_len
  * @h: the head of the list
  * Return: the nbr of nodes in the list
  **/
 size_t listint_len(const listint_t *h)
 {
-    size_t count;
+size_t count;
 
-    for (count = 0; h != NULL; count++)
-    h = h->next;
-    return (count);
+for (count = 0; h != NULL; count++)
+h = h->next;
+return (count);
 }
