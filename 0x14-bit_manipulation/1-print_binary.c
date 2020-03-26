f6fdc5d0e1076ddf957ddfec0583b9eebaaf5d7a
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-26 22:44:45 +0100

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,29 +7,29 @@
  **/
 void print_binary(unsigned long int n)
 {
-    int x, y = n;
+int x, y = n;
 
-    if (n == 0)
-    {
-    printf("0");
-    return;
-    }
-    else
-    {
-        for (x = 0; (y >>= 1) > 0; x++)
-        ;
-        while(x >= 0)
-        {
-            if ((n >> x) & 1)
-            {
-                printf("1");
-            }
-            else
-            {
-                printf("0");
-            }
-            x--;
-        }
+if (n == 0)
+{
+printf("0");
+return;
+}
+else
+{
+for (x = 0; (y >>= 1) > 0; x++)
+;
+while (x >= 0)
+{
+if ((n >> x) & 1)
+{
+printf("1");
+}
+else
+{
+printf("0");
+}
+x--;
+}
 
-    }
+}
 }
