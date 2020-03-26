1363111a1dd09f03693070354a39068b7bb3820d
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-26 21:06:55 +0100

    0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * binary_to_uint - binary_to_uint
+ * @b : the character
+ * Return: the nbm in decimal
+ **/
+unsigned int binary_to_uint(const char *b)
+{
+    unsigned int res = 0, pow;
+    int i;
+    for (i = 0; b[i] != '\0';)
+    {
+        if (b[i] == '0' || b[i] == '1')
+        i++;
+        else
+        return (0);        
+    }
+    
+    pow = 1;
+    i--;
+    while (i >= 0)
+    {
+        if (b[i] == '1')
+        res += pow;
+        pow *= 2;
+        i--;
+    }
+
+    return (res);
+}
