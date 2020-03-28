55d6df4282643a3ac8aa286eb491e5eac53ccea3
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-28 21:12:57 +0100

    5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,30 +1,30 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
- * get_bit - get_bit
- * @n : the nbr
- * @index : the index
+ * flip_bits - flip_bits
+ * @n : the first nbr
+ * @m : the second nbr
  * Return: the bit
  **/
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    int count = 0;
+int count = 0;
 
-    while(n > 0)
-    {
-        if ((n & 1) != (m & 1))
-        {
-            n >>= 1;
-            m >>= 1;
-            count++;
-        }
-        else
-        {
-            n >>= 1;
-            m >>= 1;
-        }
-        
-    }
+while (n > 0)
+{
+if ((n & 1) != (m & 1))
+{
+n >>= 1;
+m >>= 1;
+count++;
+}
+else
+{
+n >>= 1;
+m >>= 1;
+}
 
-    return (count);
+}
+
+return (count);
 }
