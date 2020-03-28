65f2f78245395ea4e7f83744b7140bca4aa5054a
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-29 00:19:36 +0100

    100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,7 +2,7 @@
 /**
  * print_before_main - function that prints str before the main
  */
-void __attribute__((constructor)) print_before_main(void)
+void print_before_main(void) __attribute__((constructor))
 {
 printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n");
