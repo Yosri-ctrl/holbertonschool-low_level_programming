8a965b81322656fec3fff2f5a439ac6c42b71236
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-31 18:07:01 +0100

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,28 +8,28 @@
 **/
 int create_file(const char *filename, char *text_content)
 {
-    int fp, count, i;
+int fp, count, i;
 
-    if (filename == NULL)
-    return (-1);
-    fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fp == -1)
-    return (-1);
+if (filename == NULL)
+return (-1);
+fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+if (fp == -1)
+return (-1);
 
-    if (text_content == NULL)
-	{
-		close(fp);
-		return (1);
-	}
-    else
-    {
-        for (i = 0; text_content[i] != '\0'; i++)
-		;
-		count = write(fp, text_content, i);
-		if (count == -1)
-			return (-1);
-    }
+if (text_content == NULL)
+{
+close(fp);
+return (1);
+}
+else
+{
+for (i = 0; text_content[i] != '\0'; i++)
+;
+count = write(fp, text_content, i);
+if (count == -1)
+return (-1);
+}
 
-    close(fp);
-    return (1);
+close(fp);
+return (1);
 }
