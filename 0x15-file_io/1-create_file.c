f9ddcabb898a0e5f15fe6b05a11dd68407d139c3
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-31 18:06:06 +0100

    1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+*read_textfile - read a text file and print it the stander output
+*@filename: the file to treat
+*@letters: the nbr of characters in the filename
+*Return: the actuale size of letters printed
+**/
+int create_file(const char *filename, char *text_content)
+{
+    int fp, count, i;
+
+    if (filename == NULL)
+    return (-1);
+    fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (fp == -1)
+    return (-1);
+
+    if (text_content == NULL)
+	{
+		close(fp);
+		return (1);
+	}
+    else
+    {
+        for (i = 0; text_content[i] != '\0'; i++)
+		;
+		count = write(fp, text_content, i);
+		if (count == -1)
+			return (-1);
+    }
+
+    close(fp);
+    return (1);
+}
