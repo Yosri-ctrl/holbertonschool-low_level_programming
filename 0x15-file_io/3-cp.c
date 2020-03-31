d836a10de42a9f21c764cc0edac823d445ccf5cd
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-31 22:15:15 +0100

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,11 +16,11 @@ if (ac != 3)
 {dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"); exit(97); }
 
 ff = open(av[1], O_RDONLY);
-if (ft == -1)
+if (ff == -1)
 {dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]); exit(98); }
 
 ft = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-if (ff == -1)
+if (ft == -1)
 {dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]); exit(99); }
 
 while (rcheck)
