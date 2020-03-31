a3c8822911417ccc82d1432a68169d651a5296b6
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-31 22:28:52 +0100

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,28 +17,32 @@ if (ac != 3)
 
 ff = open(av[1], O_RDONLY);
 if (ff == -1)
-{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]); exit(98); }
-
+{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+exit(98); }
 ft = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 if (ft == -1)
-{dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]); exit(99); }
-
+{dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+exit(99); }
 while (rcheck)
 {
 rcheck = read(ff, buff, size);
 if (rcheck == -1)
-{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]); exit(98); }
+{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+exit(98); }
 if (rcheck > 0)
 {
 wcheck = write(ft, buff, rcheck);
 if (wcheck == -1)
-{dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]); exit(99); }
+{dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+exit(99); }
 }
 }
 
 if (close(ff) == -1)
-{dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ff); exit(100); }
+{dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ff);
+exit(100); }
 if (close(ft) == -1)
-{dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ft); exit(100); }
+{dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ft);
+exit(100); }
 return (0);
 }
