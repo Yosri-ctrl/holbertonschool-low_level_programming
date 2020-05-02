34639a597d838ad554dff8b9e23117d2d78a6cc3
Yosri-ctrl <yosribouabid@gmail.com>
2020-05-02 15:41:11 +0100

    task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+*dlistint_len - count the number of element of a list
+*@h: the list to treat
+*Return: the number of the nodes
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *node = h;
+	int count;
+
+	while (node)
+	{
+		node = node->next;
+		count++;
+	}
+	return (count);
+}
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -21,5 +22,6 @@ typedef struct dlistint_s
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
 
 #endif
