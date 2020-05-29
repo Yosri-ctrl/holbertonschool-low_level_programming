73ec41a23479bf1d9f2024379f8ba0a8b188d484
Yosri-ctrl <yosribouabid@gmail.com>
2020-05-29 13:43:56 +0100

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,14 +6,23 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *head;
-    unsigned long int i;
+	hash_table_t *head;
+	unsigned long int i = 0;
 
-    head = malloc(sizeof(hash_table_t));
-    head->array = malloc(sizeof(hash_table_t) * size);
+	head = malloc(sizeof(hash_table_t) * 1);
+	if (head == NULL)
+		return NULL;
 
-    for (i = 0; i < size; i++)
-        head->array[i] = NULL;
-    return (head);
+	head->array = malloc(sizeof(hash_table_t *) * size);
+	if (head->array == NULL)
+	{
+		free(head);
+		return NULL;
+	}
+
+	for (; i < size; i++)
+		head->array[i] = NULL;
+
+	return (head);
 }
 
