39fadd5083762a3ef7878147a4a6c62c7ce4b12f
Yosri-ctrl <yosribouabid@gmail.com>
2020-05-29 20:53:01 +0100

    task 2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -7,8 +7,7 @@
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	const unsigned char i;
+	const unsigned char i = hash_djb2(key) % size;
 
-	i = hash_djb2(key) % size;
 	return (i);
 }
