1f5663467d0e9fc151f6d505859f6d53f2158cf1
Yosri-ctrl <yosribouabid@gmail.com>
2020-07-03 16:30:50 +0100

    task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Island Perimeter 
+"""
+
+
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of an island
+    """
+    sum = 0
+    for i in range(len(grid)):
+        """print(grid[i])"""
+        for j in range(i+1):
+            """print(grid[i][j])"""
+            if grid[i][j] == 1:
+                if grid[i][j+1] == 0:
+                    sum += 1
+                if grid[i][j-1] == 0:
+                    sum += 1
+                if grid[i+1][j] == 0:
+                    sum += 1
+                if grid[i-1][j] == 0:
+                    sum += 1
+    return sum
