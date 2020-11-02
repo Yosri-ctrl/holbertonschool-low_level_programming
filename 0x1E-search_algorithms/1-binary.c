26e65b320032732c34f8882daabf9ff4220b9c27
Yosri-ctrl <yosribouabid@gmail.com>
2020-11-02 18:54:22 +0100

    Task 1

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,45 @@
+#include "search_algos.h"
+/**
+ * binary_search - search value in array.
+ * @array: list to search in
+ * @size: the size of the list
+ * @value: value to search for in list
+ * Return: index of the value in array
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	return (result(array, 0, size - 1, value));
+}
+
+/**
+* result - treat the array
+ * @array: list to search in
+ * @start: the size of the list
+ * @end: the end of the array
+ * @value: value to search for in list
+ * Return: index of the value in array
+*/
+int result(int array[], int start, int end, int value)
+{
+	while (start <= end)
+	{
+		int m = start + (end - start) / 2, l = start;
+
+		printf("Searching in array: ");
+		for (; l <= end; l++)
+		{
+			printf("%d", array[l]);
+			if (l < end)
+				printf(", ");
+		}
+		printf("\n");
+
+		if (array[m] == value)
+			return (m);
+
+		if (array[m] < value)
+			start = m + 1;
+		else
+			end = m - 1;
+	return (-1);
+}
